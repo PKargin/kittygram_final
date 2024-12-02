1e8aab1bccc6ea81ff99ff4e34f901a71b56c957
Chiken-Kitchen <filkargin@yandex.ru>
2024-12-02 22:59:01 +0300

    jsdfjk

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
 load_dotenv(env_path)
 
-SECRET_KEY = os.getenv('SECRET_KEY', '') 
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 DEBUG = os.getenv('DJANGO_DEBUG', '') != False
 
